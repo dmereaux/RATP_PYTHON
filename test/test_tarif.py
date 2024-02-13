105b89a7612c7c0b995c1a4401b48baf11186d4f
dominique mereaux <dmereaux@gmail.com>
2024-02-13 09:36:11 +0100

    setUp

--- test/test_tarif.py
+++ test/test_tarif.py
@@ -1,9 +1,17 @@
 import sys
 sys.path.append('src')
 from calcul_tarif import calcul_tarif
+import pytest
+
+@pytest.fixture
+def fix_setUpTearDown():
+    print("before")
+    yield
+    print("after")
 
 
-def test_one():
+def test_one(fix_setUpTearDown):
+    print("test")
     calcul = calcul_tarif()
     y=calcul.tarif(4,True)
     assert y == 1.5
